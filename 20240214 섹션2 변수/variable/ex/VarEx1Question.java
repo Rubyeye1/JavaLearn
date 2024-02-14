0a505c4e6c33b0fd9237255a1463f2f7ab918ee0
박형욱 <118232537+Rubyeye1@users.noreply.github.com>
2024-02-14 20:45:36 +0900

    Add files via upload

---
+++ 20240214 섹션2 변수/variable/ex/VarEx1Question.java
@@ -0,0 +1,12 @@
+package variable.ex;
+
+public class VarEx1Question {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 3;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+    }
+}
